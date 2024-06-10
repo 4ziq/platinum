8f0427aeb5e15bfdfae584866bb9e3bef2fd9464
4ziq <96971890+4ziq@users.noreply.github.com>
2024-06-10 17:32:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 //route system main page
-Route::get('/', [PlatinumController::class, 'index'])->middleware('auth');
+Route::get('/', [PlatinumController::class, 'index']); //->middleware('auth')
 
 //Show register platinum/create form
 Route::get('/register-user', [UserController::class, 'create']);
@@ -14,8 +14,10 @@ Route::get('/register-user', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
 
 //show login form
-Route::get('/login', [UserController::class, 'login'])->middleware('guest');
+Route::get('/login', [UserController::class, 'login']); //->middleware('guest')
 
 // log in user
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 
+//shows profile page
+Route::get('/profile', [UserController::class, 'profile']);
